8d8ce198a738cfd7290082286ffcce5d44cc900a
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2023-04-16 18:19:58 +0300

    editor fixes

--- src/components/settings/EditorSettings.vue
+++ src/components/settings/EditorSettings.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
-  import { ref, unref } from 'vue'
-import config from '../../helpers/config';
+  import { ref } from 'vue'
+  import config from '../../helpers/config'
+
 
   const open = ref(false)
+
+
+  const settings = {
+    compile: {
+      oncommand: 'по команде',
+      instantly: 'моментально',
+      '1sec': 'после 1 сек. покоя',
+      '3sec': 'после 3 сек. покоя',
+      '5sec': 'после 5 сек. покоя',
+    }
+  }
 </script>
 <template>
-  <button class="btn-sm w-8 h-8 btn" @click="open = !open">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="hover:rotate-180 transition-all duration-500 w-6 h-6 min-w-[24px] min-h-[24px]" stroke-width="1.5" stroke="white" >
+  <button class="group btn-sm w-8 h-8 btn" @click="open = !open">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="group-hover:rotate-180 transition-all duration-500 w-6 h-6 min-w-[24px] min-h-[24px]" stroke-width="1.5" stroke="white" >
       <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
       <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
     </svg>
@@ -19,11 +31,7 @@ import config from '../../helpers/config';
         <div class="flex w-full items-center justify-between mt-8">
           <span v-text="'режим компиляции'" class="font-medium" />
           <select v-model="config.compile.mode" class="select select-bordered">
-            <option value="oncommand" v-text="'по команде'" />
-            <option value="instantly" v-text="'моментально'" />
-            <option value="1sec" v-text="'после 1 сек. покоя'" />
-            <option value="3sec" v-text="'после 3 сек. покоя'" />
-            <option value="5sec" v-text="'после 5 сек. покоя'" />
+            <option v-for="(compileOption, name, i) in settings.compile" :key="compileOption" :value="name" v-text="compileOption" />
           </select>
         </div>
       </div>
